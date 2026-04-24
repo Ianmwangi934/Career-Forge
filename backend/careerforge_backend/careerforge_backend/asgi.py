3d9c4bf9b99add62b08487b4927c61c5f8fcd5e0
Ianmwangi934 <ianmurigu4@gmail.com>
2026-04-24 06:20:22 -0400

    Initial carret forge backend setup

---
+++ backend/careerforge_backend/careerforge_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for careerforge_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'careerforge_backend.settings')
+
+application = get_asgi_application()
