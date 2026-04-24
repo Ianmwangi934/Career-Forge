212f3fc1c5639f08d102829af107dec8effb8f6b
Ianmwangi934 <ianmurigu4@gmail.com>
2026-04-24 06:58:47 -0400

    Initial set up for the project

--- backend/careerforge_backend/accounts/views.py
+++ backend/careerforge_backend/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+from .serializers import RegisterSerializer
+from rest_framework
 
 # Create your views here.
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class UserProfile(RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
