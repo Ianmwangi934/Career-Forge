6988ee12cf374f9dede9b81e894520fcbf2a6749
Ianmwangi934 <ianmurigu4@gmail.com>
2026-04-27 05:22:25 -0400

    Worked on authetnication and home page in the frontend

--- backend/careerforge_backend/accounts/views.py
+++ backend/careerforge_backend/accounts/views.py
@@ -16,6 +16,42 @@ from rest_framework_simplejwt.tokens import RefreshToken
 class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+
+            #Generate tokens
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
+            response = Response(
+                {"message": "User Registered and Logged in Successfully."},
+                status=status.HTTP_201_CREATED
+            )
+            # Set access token cookie
+            response.set_cookie(
+                key=settings.JWT_COOKIE_NAME,
+                value=access_token,
+                httponly=True,
+                secure=settings.JWT_COOKIE_SECURE,
+                samesite=settings.JWT_COOKIE_SAMESITE
+            )
+
+            # Set refresh token cookie
+            response.set_cookie(
+                key=settings.JWT_REFRESH_COOKIE_NAME,
+                value=refresh_token,
+                httponly=True,
+                secure=settings.JWT_COOKIE_SECURE,
+                samesite=settings.JWT_COOKIE_SAMESITE
+            )
+
+            return response
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class UserProfile(RetrieveAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
@@ -61,4 +97,4 @@ class LoginView(APIView):
             samesite=settings.JWT_COOKIE_SAMESITE
         )
 
-        return Response 
+        return response 
---
+++ frontend/CareerForge_frontend/src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import "./Home.css"
+import {useNavigate} from "react-router-dom"
+
+const Home = () =>{
+    const navigate = useNavigate()
+
+    return (
+        <div className="home-container">
+            <div className="home-card">
+                <h1 className="logo">CareerForge</h1>
+
+                <p className="tagline">
+                    Shaping your career intentionally.
+                </p>
+                <p className="subtitle">
+                    AI-powered resume tailoring, cover letters, and interview prep — all in one place.
+                </p>
+            
+                <div className="buttons">
+                    <button onClick={() => navigate("/login")} className="btn primary">
+                        Login
+                    </button>
+                    <button onClick={() => navigate("/register")} className="btn secondary">
+                        Register
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home
